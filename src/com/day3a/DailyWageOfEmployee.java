25261fa39e84938c86e0eb392bc5a173df8da10b
prasadjamdade <prasadjamdade@hotmail.com>
2021-09-08 02:04:47 +0530

    Calculation of Daily Wage of Employee added

---
+++ src/com/day3a/DailyWageOfEmployee.java
@@ -0,0 +1,9 @@
+package com.day3a;
+
+public class DailyWageOfEmployee {
+    public static void main(String[] args) {
+        int wagePerHour = 20;
+        int hoursPerDay = 8;
+        System.out.println("The Daily Wage of Emaployee is: " + wagePerHour*hoursPerDay + " INR");
+    }
+}
