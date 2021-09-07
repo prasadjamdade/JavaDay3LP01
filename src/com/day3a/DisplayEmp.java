199babf657fbff11329147a88348610da738015b
prasadjamdade <prasadjamdade@hotmail.com>
2021-09-08 00:47:42 +0530

    First Commit

---
+++ src/com/day3a/DisplayEmp.java
@@ -0,0 +1,7 @@
+package com.day3a;
+
+public class DisplayEmp {
+    public static void main(String[] args) {
+        System.out.println("---Welcome to Employee Wage Computation Program---");
+    }
+}
