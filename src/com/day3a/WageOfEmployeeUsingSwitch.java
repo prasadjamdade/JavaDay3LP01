204c2311c0a037bdcb77123eaeadc20d082665a9
prasadjamdade <prasadjamdade@hotmail.com>
2021-09-08 11:39:10 +0530

    To calculate Wage of Employees using Switch case WageOfEmployeeUsingSwitch.java file refactored.

--- src/com/day3a/WageOfEmployeeUsingSwitch.java
+++ src/com/day3a/WageOfEmployeeUsingSwitch.java
@@ -1,11 +1,23 @@
 package com.day3a;
 
+import java.util.Scanner;
+
 public class WageOfEmployeeUsingSwitch {
     public static void main(String[] args) {
+
+        Scanner scanObj = new Scanner(System.in);
+        System.out.println("Please enter 1 for full time Employee Wage.\n Please enter 2 for part time Employee Wage. ");
         int wagePerHour = 20;
         int hoursPerDay = 8;
         int hoursPartTime = hoursPerDay/2;
-        System.out.println("The Daily Wage of Employee is: " + wagePerHour*hoursPerDay + " INR");
-        System.out.println("The Daily Wage of Part Time Employee is: " + wagePerHour*hoursPartTime + " INR");
+        int option = scanObj.nextInt();
+        switch(option)
+        {
+            case 1 : System.out.println("The Daily Wage of Full Time Employee is: " + wagePerHour * hoursPerDay + " INR");
+            break;
+            case 2 : System.out.println("The Daily Wage of Part Time Employee is: " + wagePerHour * hoursPartTime + " INR");
+            break;
+            default : System.out.println("Invalid Entry...");
+        }
     }
 }
