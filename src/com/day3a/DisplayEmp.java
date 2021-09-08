4a27f03cae758b7afc1c35f69360d47d9d7e684b
prasadjamdade <prasadjamdade@hotmail.com>
2021-09-08 11:18:21 +0530

    To calculate Wage of Employees using Switch case WageOfEmployeeUsingSwitch.java file added, and deleted .class files

--- src/com/day3a/DisplayEmp.java
+++
@@ -1,7 +0,0 @@
-package com.day3a;
-
-public class DisplayEmp {
-    public static void main(String[] args) {
-        System.out.println("---Welcome to Employee Wage Computation Program---");
-    }
-}
---
+++ src/com/day3a/WageOfEmployeeUsingSwitch.java
@@ -0,0 +1,11 @@
+package com.day3a;
+
+public class WageOfEmployeeUsingSwitch {
+    public static void main(String[] args) {
+        int wagePerHour = 20;
+        int hoursPerDay = 8;
+        int hoursPartTime = hoursPerDay/2;
+        System.out.println("The Daily Wage of Employee is: " + wagePerHour*hoursPerDay + " INR");
+        System.out.println("The Daily Wage of Part Time Employee is: " + wagePerHour*hoursPartTime + " INR");
+    }
+}
